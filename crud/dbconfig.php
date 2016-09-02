c58ce71c8109927010910760c3f91364126f8823
andressan95 <cristobal.sanchezac@gmail.com>
2016-09-02 15:29:58 -0500

    Editado 02/09/2016

--- crud/dbconfig.php
+++ crud/dbconfig.php
@@ -1,9 +1,18 @@
 <?php
+
+
+	/*
+		
+		Database=db_administracion;
+		Data Source=ca-cdbr-azure-central-a.cloudapp.net;
+		User Id=be00176cf03501;
+		Password=b129547f
+		*/
 error_reporting(0);
-$host = "localhost";
-$user = "root";
-$password = "";
-$datbase = "dbtuts";
+$host = "ca-cdbr-azure-central-a.cloudapp.net";
+$user = "be00176cf03501";
+$password = "b129547f";
+$datbase = "db_administracion";
 mysql_connect($host,$user,$password);
 mysql_select_db($datbase);
 ?>
