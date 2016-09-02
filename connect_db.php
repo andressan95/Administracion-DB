d30b89d008327b0c8b32ca09b64920eb334dab0e
andressan95 <cristobal.sanchezac@gmail.com>
2016-09-01 23:38:01 -0500

    edit

--- connect_db.php
+++ connect_db.php
@@ -1,7 +1,13 @@
 <?php
+/*
+		
+		Database=db_administracion;
+		Data Source=ca-cdbr-azure-central-a.cloudapp.net;
+		User Id=be00176cf03501;
+		Password=b129547f
+		*/
 
-
-		$mysqli = new MySQLi("localhost", "root","", "academ");
+		$mysqli = new MySQLi("ca-cdbr-azure-central-a.cloudapp.net", "be00176cf03501","b129547f", "db_administracion");
 		if ($mysqli -> connect_errno) {
 			die( "Fallo la conexión a MySQL: (" . $mysqli -> mysqli_connect_errno() 
 				. ") " . $mysqli -> mysqli_connect_error());
