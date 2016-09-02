04b5990103eb2c9e3ad809975bb23d8c942ab086
andressan95 <cristobal.sanchezac@gmail.com>
2016-09-02 16:01:55 -0500

    Editado 02/09/2016

--- crud/add_data.php
+++ crud/add_data.php
@@ -64,57 +64,7 @@ if(isset($_POST['btn-save']))
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <title>CRUD Operations With PHP and MySql - By Coding Cage</title>
-<script>
-			function validarNombre()
-			{
-				valor = document.getElementById("nombre").value;
-				if( valor == null || valor.length == 0 || /^\s+$/.test(valor) ) {
-					alert('Falta Llenar Nombre');
-					return false;
-				} else { return true;}
-			}
-			
-			function validarUsuario()
-			{
-				valor = document.getElementById("usuario").value;
-				if( valor == null || valor.length == 0 || /^\s+$/.test(valor) ) {
-					alert('Falta Llenar Usuario');
-					return false;
-				} else { return true;}
-			}
-			
-			function validarPassword()
-			{
-				valor = document.getElementById("password").value;
-				if( valor == null || valor.length == 0 || /^\s+$/.test(valor) ) {
-					alert('Falta Llenar Password');
-					return false;
-					} else { 
-					valor2 = document.getElementById("con_password").value;
-					
-					if(valor == valor2) { return true; }
-					else { alert('Las contraseña no coinciden'); return false;}
-				}
-			}
-			
-			function validarTipoUsuario()
-			{
-				indice = document.getElementById("tipo_usuario").selectedIndex;
-				if( indice == null) {
-					alert('Seleccione tipo de usuario');
-					return false;
-				} else { return true;}
-			}
-			
-			function validar()
-			{
-				if(validarNombre() && validarUsuario() && validarPassword() && validarTipoUsuario())
-				{
-					document.registro.submit();
-				}
-			}
-			
-		</script>
+
 <link rel="stylesheet" href="style.css" type="text/css" />
 </head>
 <body>
