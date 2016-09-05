e0c304b26d077fbb7722f36c3febd9279eef1f70
andressan95 <cristobal.sanchezac@gmail.com>
2016-09-05 11:58:52 -0500

    editado 05/09/2016

--- crud/edit_data2.php
+++ crud/edit_data2.php
@@ -102,19 +102,19 @@ if(isset($_POST['btn-cancel']))
 
       <nav>
     <div class="nav-wrapper">
-      <a href="#!" class="brand-logo">Editar Registro</a>
+      <a href="#!" class="brand-logo">Logo</a>
       <a href="#" data-activates="mobile-demo" class="button-collapse"><i class="material-icons">menu</i></a>
       <ul class="right hide-on-med-and-down">
-        <li><a href="sass.html">Sass</a></li>
-        <li><a href="badges.html">Components</a></li>
-        <li><a href="collapsible.html">Javascript</a></li>
-        <li><a href="mobile.html">Mobile</a></li>
+       <li><a href="usuarios.php">Usuarios</a></li>
+        <li><a href="registro.php">Registro</a></li>
+        <li><a href="logout.php">Cerrar Sesion</a></li>
+        <li><a href="administrador.php">Administrador</a></li>
       </ul>
       <ul class="side-nav" id="mobile-demo">
-        <li><a href="sass.html">Sass</a></li>
-        <li><a href="badges.html">Components</a></li>
-        <li><a href="collapsible.html">Javascript</a></li>
-        <li><a href="mobile.html">Mobile</a></li>
+        <li><a href="usuarios.php">Usuarios</a></li>
+        <li><a href="registro.php">Registro</a></li>
+        <li><a href="logout.php">Cerrar Sesion</a></li>
+        <li><a href="administrador.php">Administrador</a></li>
       </ul>
     </div>
   </nav>
