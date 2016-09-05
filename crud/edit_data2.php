bb16e927d5a5c9ebadd3ffbb7d627a277f74f805
andressan95 <cristobal.sanchezac@gmail.com>
2016-09-05 11:46:03 -0500

    editado 05/09/2016

--- crud/edit_data2.php
+++ crud/edit_data2.php
@@ -94,17 +94,17 @@ if(isset($_POST['btn-cancel']))
 <center>
 
 
-     <div class="navbar-fixed">
     <nav>
-      <div class="nav-wrapper">
-        <a href="#!" class="brand-logo">Logo</a>
-        <ul class="right hide-on-med-and-down">
-          <li><a href="sass.html">Sass</a></li>
-          <li class="active"><a href="badges.html">Components</a></li>
-        </ul>
-      </div>
-    </nav>
-  </div>
+    <div class="nav-wrapper">
+      <a href="#!" class="brand-logo center">Logo</a>
+      <ul class="left hide-on-med-and-down">
+        <li><a href="sass.html">Sass</a></li>
+        <li><a href="badges.html">Components</a></li>
+        <li class="active"><a href="collapsible.html">JavaScript</a></li>
+      </ul>
+    </div>
+  </nav>
+        
 
 <div id="body">
 	<div id="content">
