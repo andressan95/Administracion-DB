b2d4390d81ddb11ddddc3abe56135bec592ae55e
andressan95 <cristobal.sanchezac@gmail.com>
2016-09-05 11:54:36 -0500

    editado 05/09/2016

--- crud/edit_data2.php
+++ crud/edit_data2.php
@@ -89,18 +89,32 @@ if(isset($_POST['btn-cancel']))
         <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
         <script type="text/javascript" src="materialize/js/materialize.min.js"></script>
         <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet"></link>
+        <script> 
+        
+         $( document ).ready(function(){
+              $(".button-collapse").sideNav();
+         }) 
+        </script>
 </head>
 <body>
 <center>
 
 
-    <nav>
+      <nav>
     <div class="nav-wrapper">
-      <a href="#!" class="brand-logo center">Logo</a>
-      <ul class="left hide-on-med-and-down">
+      <a href="#!" class="brand-logo">Editar Registro</a>
+      <a href="#" data-activates="mobile-demo" class="button-collapse"><i class="material-icons">menu</i></a>
+      <ul class="right hide-on-med-and-down">
         <li><a href="sass.html">Sass</a></li>
         <li><a href="badges.html">Components</a></li>
-        <li class="active"><a href="collapsible.html">JavaScript</a></li>
+        <li><a href="collapsible.html">Javascript</a></li>
+        <li><a href="mobile.html">Mobile</a></li>
+      </ul>
+      <ul class="side-nav" id="mobile-demo">
+        <li><a href="sass.html">Sass</a></li>
+        <li><a href="badges.html">Components</a></li>
+        <li><a href="collapsible.html">Javascript</a></li>
+        <li><a href="mobile.html">Mobile</a></li>
       </ul>
     </div>
   </nav>
