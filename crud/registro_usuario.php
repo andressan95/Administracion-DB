85acc20ab4cfca2098b0117c4ca4bc63079bde98
andressan95 <cristobal.sanchezac@gmail.com>
2016-09-07 23:08:44 -0500

    editado dise;o 07/09/2016

--- crud/registro_usuario.php
+++ crud/registro_usuario.php
@@ -208,12 +208,8 @@
     </nav>
 <body>
     
-    <div class="center-align">
             <form class="col s12" id="registro" name="registro" action="<?php $_SERVER['PHP_SELF']; ?>" method="POST">
-            <ul class="collapsible popout" data-collapsible="accordion">
-                <li>
-                    <div class="collapsible-header"><i class="material-icons">filter_drama</i>Registro de Inicio de Sesion</div>
-                    <div class="collapsible-body">
+            
                         <div class="centered row">
                             <div class="row">
                                 <div class="input-field col s4">
@@ -235,10 +231,7 @@
                             </div>
                         </div>
                     </div>
-                </li>
-                <li>
-                    <div class="collapsible-header"><i class="material-icons">whatshot</i>Datos Personales</div>
-                    <div class="collapsible-body">
+               
                         <div class="row center">
                             <div class="row">
                                 <div class="input-field col s4">
@@ -275,12 +268,7 @@
 
 
                     </div>
-                </li>
-                <li>
-                    <div class="collapsible-header"><i class="material-icons">place</i>Direccion</div>
-                    <div class="collapsible-body">
-
-                        <div class="center-on-small-only row">
+                   <div class="center-on-small-only row">
                             <div class="input-field col s4">
                                 <input id="ciudad" name="ciudad" type="text" class="validate" length="50">
                                     <label for="ciudad">Ciudad</label>
@@ -305,10 +293,7 @@
                             </div>
                         </div>
 
-                    </div>
-                </li>
-            </ul>
-
+                   
 
            
                 <input name="tipo_usuario" id="idtipo_usuario" type="radio" value="2" checked />
