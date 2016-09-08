5b618d031f88af36c3840c985f02046f51592bcc
andressan95 <cristobal.sanchezac@gmail.com>
2016-09-07 21:25:37 -0500

    editado dise;o 07/09/2016

--- crud/registro_usuario.php
+++ crud/registro_usuario.php
@@ -133,6 +133,11 @@
         
          $( document ).ready(function(){
               $(".button-collapse").sideNav();
+            
+                    $('.collapsible').collapsible({
+                      accordion : false // A setting that changes the collapsible behavior to expandable instead of the default accordion style
+                    });
+
          }) 
         </script>
 </head>
